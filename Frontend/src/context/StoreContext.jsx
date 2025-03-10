aa30fc359b4a47a99bb8f8edc1d158e2f749657e
mahendrapabbathi <pabbathimahendra8@gmail.com>
2025-03-10 09:18:57 +0530

    Update StoreContext.jsx

--- Frontend/src/context/StoreContext.jsx
+++ Frontend/src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null)
 
 const StoreContextProvider = (props)=>{
 
-    const url = "http://localhost:4000";
+    const url = "https://unlockedu-backend-j24v.onrender.com";
     const [token,setToken] = useState("");
 
     // for do not change the sign out button when we refresh
