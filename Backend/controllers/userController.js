da68e53ebd1284359f6318c0d39ac4e5d92eed61
Mahendra Pabbathi <pabbathimahendra8@gmail.com>
2025-03-10 09:10:12 +0530

    Third commit

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -3,7 +3,7 @@
 
 import userModel from "../models/userModel.js";
 import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import validator from "validator";
 import { sendEmail } from "../emailSending/sendEmail.js";
 
